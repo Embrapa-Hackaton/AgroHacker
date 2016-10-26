88f84604430390fa015d173b3034bad155ae2228
Victor Arnaud <victorhad@gmail.com>
2016-10-26 17:49:42 -0200

    About activity

---
+++ app/src/main/java/hackathon/embrapa/agrohacker/controller/AboutActivity.java
@@ -0,0 +1,34 @@
+package hackathon.embrapa.agrohacker.controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import hackathon.embrapa.agrohacker.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_back, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/hackathon/embrapa/agrohacker/controller/MapActivity.java
+++ app/src/main/java/hackathon/embrapa/agrohacker/controller/MapActivity.java
@@ -168,7 +168,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Toast.makeText(MapActivity.this, "Em construção", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.menu_about:
-                Toast.makeText(MapActivity.this, "Em construção", Toast.LENGTH_SHORT).show();
+                Intent intentAboutActivity = new Intent(MapActivity.this, AboutActivity.class);
+                startActivity(intentAboutActivity);
                 break;
             case R.id.menu_exit:
                 finish();
